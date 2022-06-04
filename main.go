27a1e4b826f2913bf383ea26ea287e8fe804e90c
ESONHUGH <32677240+Esonhugh@users.noreply.github.com>
2022-06-04 13:34:20 +0000

    feat(more exp): add tests.

--- main.go
+++ main.go
@@ -20,7 +20,7 @@ func MakeToken(code string) []*lexer.Token {
 	return tokens
 }
 
-// 
+// Grammar 语法分析 同时输出结果 递归下降 也就是自顶向下解析
 func Grammar(tokens []*lexer.Token) {
 	gram, correct := grammar.Analyse(tokens)
 	if !correct {
@@ -29,6 +29,7 @@ func Grammar(tokens []*lexer.Token) {
 	servicePrint.PrintGrammar(gram)
 }
 
+// GrammarLL1 语法分析 同时输出结果 LL1 解析
 func GrammarLL1(tokens []*lexer.Token) {
 	gram, correct := grammarLL1.Analyze(tokens, "E->TG\nG->ATG|&\nT->FS\nS->MFS|&\nF->(E)|i\nA->+|-\nM->*|/", "E")
 	if !correct {
@@ -37,7 +38,13 @@ func GrammarLL1(tokens []*lexer.Token) {
 	servicePrint.PrintGrammarLL1(gram)
 }
 
+// 实验 
 func main() {
+	main_proxy()
+}
+
+func main_proxy(){
+	// LL1 Grammar Parser
 	//tokens := MakeToken("i*(i-i)/(i+i)*((i+i-i*i/i))")
 	//tokens := MakeToken("i+i")
 	tokens := MakeToken("i*i**")
@@ -47,4 +54,12 @@ func main() {
 	//tokens := MakeToken("(i-i)(i/i)")
 	//Grammar(tokens)
 	GrammarLL1(tokens)
+	
+	// 递归下降
+	Grammar(tokens)
+	
+	// SysY 分析
+	tokens = MakeToken("for(int i=1;i<=10;i++) begin\na_b++;#zszszszszs\nb_C--;#zszszszszs\nB123:=1234567;\na=@;\n123a=0;\na.b;\nend\n")
+	servicePrint.PrintToken(tokens)
+
 }
