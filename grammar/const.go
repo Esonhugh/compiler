7509a5c13f855302027c9c6f872d3f459a94582f
ESONHUGH <32677240+Esonhugh@users.noreply.github.com>
2022-06-04 07:34:44 +0000

    doc(grammar): godoc

--- grammar/const.go
+++ grammar/const.go
@@ -1,5 +1,10 @@
+/*
+Package grammar 一个简单语法分析器包 
+
+*/
 package grammar
 
+// Procduction 产生式语法定义
 type Production struct {
 	Type   string
 	Target string
--- grammar/grammar.go
+++ grammar/grammar.go
@@ -9,15 +9,36 @@ import (
 	"strings"
 )
 
+// EndToken 句子终结符
 const EndToken = "$"
 
+// Grammar 对象定义了一个语法分析需要的全部对象 以及他的分析器
 type Grammar struct {
-	*util.Stream
-	productions map[string][]string
-	endToken    string
-	tokens      *util.Queue
+	*util.Stream                     // Stream is the stream of the sentence
+	productions  map[string][]string // productions is the production map use the "string"=>"string1" "string"=>"string2" ...
+	endToken     string              // endToken is the end of the sentence
+	tokens       *util.Queue         // tokens is the queue of the sentence
 }
 
+/* makeProductions 创建产生式的映射关系 map
+由于 是单个字符匹配 所以 E' 被定义为 G
+替换如下:
+在文件 [utils/transfer/transfer.go] 中进行替换
+
+E' ==> G 
+T' ==> S
+& ==> ε
+
+原始的产生式如下
+
+E→TE’
+E’→ATE’|ε
+T→FT’
+T’→MFT’ |ε
+F→(E) | i
+A → + | -
+M → * | /
+*/
 func makeProductions() map[string][]string {
 	res := make(map[string][]string)
 	res["E"] = []string{"TG"}
@@ -30,7 +51,9 @@ func makeProductions() map[string][]string {
 	return res
 }
 
+// Analyse 语法分析器主函数
 func Analyse(raw []*lexer.Token) ([]*Production, bool) {
+	// 从 E 开始 E 即为开始符号 到 EndToken 为句子结束符号
 	grm := NewGrammar(raw, bytes.NewBufferString("E"), EndToken)
 	productions, success := grm.Analyse(1)
 	if !success {
@@ -56,9 +79,7 @@ func NewGrammar(token []*lexer.Token, r io.Reader, et string) *Grammar {
 	return &Grammar{Stream: s, endToken: et, productions: makeProductions(), tokens: q}
 }
 
-func (g *Grammar) frontMatch() bool {
-	return true
-}
+
 
 func (g *Grammar) GetNextOrigin() string {
 	c := g.Stream.Next()
@@ -69,6 +90,8 @@ func (g *Grammar) GetNextOrigin() string {
 	return c
 }
 
+// isEndType judge the one word is terminal or not.
+// EndType is the unspilttable token
 func (g *Grammar) isEndType(r string) bool {
 	dic := map[string]bool{
 		"i": true,
@@ -83,10 +106,14 @@ func (g *Grammar) isEndType(r string) bool {
 	return dic[r]
 }
 
+// Analyse the grammar
+// count is the 
 func (g *Grammar) Analyse(count int) (res []*Production, canKill bool) {
 
 	// 当前推导式头部
 	origin := g.GetNextOrigin()
+
+	// 当前推导到空字符
 	if origin == "&" {
 		res = append(res, &Production{
 			Type:   "kill",
@@ -95,15 +122,15 @@ func (g *Grammar) Analyse(count int) (res []*Production, canKill bool) {
 		g.tokens.PushFront(&lexer.Token{})
 		return res, true
 	}
+
+	// 当前产生式首位为终结符
 	if g.isEndType(origin) {
-		// 当前产生式首位为终结符
 		c := g.tokens.Front()
 		if c == nil {
 			return res, false
 		}
 		currentToken := c.(*lexer.Token)
 		switch origin {
-
 		case "i":
 			{
 				if !currentToken.IsValue() {
@@ -190,9 +217,12 @@ func (g *Grammar) Analyse(count int) (res []*Production, canKill bool) {
 			}
 		}
 	}
+	
+	// 选择一个可用的产生式子
 	canUse := g.productions[origin]
 	var killToken []*lexer.Token
 	for i := 0; i < len(canUse); i++ {
+		// 倒序插入
 		for _, s := range reverse(strings.Split(canUse[i], "")) {
 			g.Stream.PutBack(s)
 		}
@@ -246,6 +276,7 @@ func (g *Grammar) Analyse(count int) (res []*Production, canKill bool) {
 	return res, false
 }
 
+// reverse 反转字符串
 func reverse(s []string) []string {
 	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
 		s[i], s[j] = s[j], s[i]
@@ -253,6 +284,7 @@ func reverse(s []string) []string {
 	return s
 }
 
+// reverseAny 
 func reverseAny(s []*lexer.Token) []*lexer.Token {
 	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
 		s[i], s[j] = s[j], s[i]
@@ -260,6 +292,8 @@ func reverseAny(s []*lexer.Token) []*lexer.Token {
 	return s
 }
 
+
+// PrintToken 打印 token
 func (g *Grammar) PrintToken() {
 	e := g.tokens.FrontRaw()
 	for {
@@ -270,5 +304,5 @@ func (g *Grammar) PrintToken() {
 			break
 		}
 	}
-	fmt.Println("$\n ")
+	fmt.Println("$\n ") // 结束符
 }
